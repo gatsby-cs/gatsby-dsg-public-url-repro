a5b369374924418c0563c43bf1b9738e5ffd9b3c
pieh <misiek.piechowiak@gmail.com>
2022-10-27 14:42:14 +0200

    add ssr page

---
+++ src/pages/ssr.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+
+export default function SSRPage({ data, serverData }) {
+  return (
+    <main>
+      SSR page
+      <ul>
+        {data.allFile.nodes.map(({ name, publicURL }) => (
+          <li key={name}>
+            <img src={publicURL} />
+            <p>{publicURL}</p>
+          </li>
+        ))}
+      </ul>
+      <pre>{JSON.stringify(serverData, null, 2)}</pre>
+    </main>
+  );
+}
+
+export async function getServerData() {
+  return () => {
+    props: {
+      wat: true;
+    }
+  };
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+        publicURL
+      }
+    }
+  }
+`;
